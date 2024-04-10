89a10572b02261f7f6b0f7d18d8047cad8088de1
Abdihafith <abdyhafith@gmail.com>
2024-04-10 11:37:38 +0300

    code challenge

--- isPrime.js
+++ isPrime.js
@@ -1,28 +1,22 @@
-// Define the isPrime function
-function isPrime(num) {
-    // If the number is less than or equal to 1, it's not prime
-    if (num <= 1) return false;
-    // If the number is 2 or 3, it's prime
-    if (num <= 3) return true;
-    // If the number is divisible by 2 or 3, it's not prime
-    if (num % 2 === 0 || num % 3 === 0) return false;
-
-    let i = 5;
-    while (i * i <= num) {
-        if (num % i === 0 || num % (i + 2) === 0) return false;
-        i += 6;
+function isPrime(number) {
+    // 2 is the 1st prime number so anything below that is not a prime number
+  if (number < 2) {
+    return false;
+  }
+//   2 will be like our first index (just like 0) and our loop will iterate between 2 and the number provided
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {   // if any of the numbers divide the given even number(2) then they are not prime numbers
+      return false;
     }
-    // If the conditions are not met, the number is prime
-    return true;    
+  }
+//   If no such divisor is found,return true meaning it is a prime number.
+  return true;  
+}
+// filterPrimeNumber takes in an array of numbers and filters out the prime numbers using the isPrime function. 
+function filterPrimeNumber(array) {
+  return array.filter((number) => isPrime(number));   // It returns a new array containing only the prime numbers.
 }
 
-// Define the filterPrimes function
-function filterPrimes(numbers) {
-    // Filter out prime numbers from the array
-    return numbers.filter(num => isPrime(num));
-}
-
-// Example usage:
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let primeNumbers = filterPrimes(numbers);
-console.log(primeNumbers); // Output: [2, 3, 5, 7]
+questionArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const primeNumbers = filterPrimeNumber(questionArray);
+console.log(primeNumbers);
--- swapCase.js
+++ swapCase.js
@@ -1,23 +1,12 @@
-// Define the swapCase function
-const swapCase = function(letters) {
-    // Start an empty array to store the swapped letters
-    let swappedLetters = [];
-    // Iterate through each character in the input string
-    for (let i = 0; i < letters.length; i++) {
-        // Check if the character is lowercase
-        if (letters[i] === letters[i].toLowerCase()) {
-            // If lowercase, change it to uppercase and push it to the array
-            swappedLetters.push(letters[i].toUpperCase());
-        } else {
-            // If not lowercase, change it to lowercase and push it to the array
-            swappedLetters.push(letters[i].toLowerCase());
+function swapCase(str){
+    return str.split('').map(char => {
+        if (char === char.toUpperCase()){
+            return char.toLowerCase();
+
+        }else{
+            return char.toUpperCase();
         }
-    }
-    // Print the array of swapped letters as a single string to the console
-    console.log(swappedLetters.join(''));
+    }).join('');
 }
 
-// Define a variable 'text' containing the string 'am Good'
-let text = 'am Good';
-// Call the swapCase function with the 'text' variable as input
-swapCase(text);
+console.log(swapCase("The Quick Brown Fox"))
